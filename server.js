47e1fee45b635db11851e83cca9d6a9577b899e8
Muhammad Rayyan <rayyan@example.com>
2026-06-17 23:38:31 +0500

    Use process.env.PORT || 3000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Path definitions
 const STATE_FILE = path.join(__dirname, 'data', 'auction_state.json');
